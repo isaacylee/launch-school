1d58994368835cd513012a540c46b8acfa8ecc38
Isaac Lee <izickree@gmail.com>
2020-07-04 00:34:34 -0700

    Created mortgage calculator.

---
+++ rb101/lesson_2/mortgage_calc.rb
@@ -0,0 +1,52 @@
+def prompt(msg)
+  puts ">> #{msg}"
+end
+
+def valid_input(num)
+  num.to_i.to_s == num && num.empty? == false && num.to_f > 0
+end
+
+error = "---ERROR: Must enter a positive number---"
+
+loop do
+  prompt("Let's calculate your mortgage payments!")
+
+  principal = ''
+  interest_rate = ''
+  loan_years = ''
+
+  loop do
+    prompt("Enter loan amount:")
+    principal = gets.chomp
+    break if valid_input(principal)
+    prompt(error)
+  end
+  
+  loop do
+    prompt("Enter Annual Percentage Rate (APR):")
+    interest_rate = gets.chomp
+    break if valid_input(interest_rate)
+    prompt(error)
+  end
+  
+  loop do
+    prompt("Enter loan duration (in years):")
+    loan_years = gets.chomp
+    break if valid_input(loan_years)
+    prompt(error)
+  end
+
+  annual_interest_rate = interest_rate.to_f / 100
+  monthly_interest_rate = (annual_interest_rate) / 12
+  loan_months = loan_years.to_i * 12
+  monthly_payment = principal.to_i *
+                    (monthly_interest_rate / 
+                    (1 - (1 + monthly_interest_rate)**(-loan_months))) 
+
+  prompt("Your monthly payment will be $#{monthly_payment.round(2)}.")
+  prompt("Do you want to calculate another mortgage payment? (y/n)")
+  answer = gets.chomp
+  break unless answer.downcase == ('y')
+end
+
+prompt("Goodbye!")
