b6827d5be47cdafa1a43bdf6a50ed6005acc4642
Isaac Lee <izickree@gmail.com>
2020-10-20 12:58:05 -0700

    Fixed rubocop errors for 21.rb

--- rb101/lesson_6/21.rb
+++ rb101/lesson_6/21.rb
@@ -31,26 +31,24 @@ def display_menu
   puts "  B. SPECIAL GAME MODE"
   puts "  C. QUIT"
   puts ""
-end 
-
+end
 
 def prompt(msg)
   puts ">> #{msg}"
 end
 
 def empty_lines
-  puts ''
-  puts ''
+  2.times { puts '' }
 end
 
 def shuffle_deck
-  dck = CARD_VALUES.keys.product(CARD_SUITS).map{ |card| card.join }
+  dck = CARD_VALUES.keys.product(CARD_SUITS).map(&:join)
   dck.shuffle!
 end
 
 def display_player(hand, total)
   sorted_hand = hand.clone
-  sorted_hand.sort_by! { |card| CARD_VALUES[card[0..card.size-2]] }
+  sorted_hand.sort_by! { |card| CARD_VALUES[card[0..card.size - 2]] }
   puts "You have:   #{join(sorted_hand)}  =>  #{total}"
 end
 
@@ -63,32 +61,32 @@ def display_dealer(hand)
 end
 
 def cards_total(hand)
-  value = hand.map { |card| CARD_VALUES[card[0..card.size-2]] }.reduce(0, :+)
-  if hand.map{ |card| card[0] }.include?('A')
+  value = hand.map { |card| CARD_VALUES[card[0..card.size - 2]] }.reduce(0, :+)
+  if hand.map { |card| card[0] }.include?('A')
     hand.map { |card| card[0] }.count('A').times do
       value -= 10 if value > WINNING_NUMBER
     end
   end
-  value 
+  value
 end
 
 def busted?(total)
-  total > WINNING_NUMBER 
+  total > WINNING_NUMBER
 end
 
-def round_outcome(p_hand, p_status, p_total, d_total, scores)
-  case
-  when p_status == nil
+# rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+def round_outcome(p_hand, p_status, p_total, d_total)
+  if p_status.nil?
     prompt("Dealer has dealt cards.")
-  when p_status == '1' && !busted?(p_total)
+  elsif p_status == '1' && !busted?(p_total)
     prompt("You drew: [#{p_hand.last}].")
-  when busted?(p_total)
+  elsif busted?(p_total)
     prompt('Bust! You lose!')
-  when p_total == WINNING_NUMBER
+  elsif p_total == WINNING_NUMBER
     prompt('Blackjack! You win!')
-  when busted?(d_total) || p_total > d_total
+  elsif busted?(d_total) || p_total > d_total
     prompt('You win!')
-  when p_total == d_total
+  elsif p_total == d_total
     prompt("It's a tie!")
   else
     prompt('Dealer wins!')
@@ -96,14 +94,15 @@ def round_outcome(p_hand, p_status, p_total, d_total, scores)
 end
 
 def score_keeper(p_total, d_total, scores)
-  case
-  when busted?(p_total) || !busted?(p_total) && !busted?(d_total) && d_total > p_total
+  if busted?(p_total)
     scores[:dealer] += 1
-  when p_total == WINNING_NUMBER || busted?(d_total) || p_total > d_total
+  elsif !busted?(p_total) && !busted?(d_total) && d_total > p_total
+    scores[:dealer] += 1
+  elsif p_total == WINNING_NUMBER || busted?(d_total) || p_total > d_total
     scores[:player] += 1
-  else
   end
 end
+# rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
 def game_outcome(scores)
   if scores[:player] == 5
@@ -114,33 +113,26 @@ def game_outcome(scores)
 end
 
 def display_scores(scores)
+  system 'clear'
   puts "  Player: #{scores[:player]}  |  Dealer: #{scores[:dealer]}".center(30)
+  empty_lines
 end
 
-def display_screen(p_hand, d_hand, p_status, p_total, d_total, scores)
-  system 'clear'
-  display_scores(scores)
-  empty_lines
+def display_table(p_hand, d_hand, p_total)
   display_dealer(d_hand)
   display_player(p_hand, p_total)
   empty_lines
-  round_outcome(p_hand, p_status, p_total, d_total, scores)
 end
 
-def round_display_screen(p_hand, d_hand, p_status, p_total, d_total, scores)
-  system 'clear'
-  display_scores(scores)
-  empty_lines
+def reveal_table(p_hand, d_hand, p_total, d_total)
   reveal_dealer(d_hand, d_total)
   display_player(p_hand, p_total)
   empty_lines
-  round_outcome(p_hand, p_status, p_total, d_total, scores)
 end
 
-def game_display_screen(scores)
+def display_gameover(scores)
   system 'clear'
   display_scores(scores)
-  empty_lines
   puts "GAME     ".center(30)
   puts "     OVER".center(30)
   empty_lines
@@ -150,7 +142,7 @@ end
 
 def play_again?
   answer = nil
-  loop do 
+  loop do
     prompt('Play again? (Y/N)')
     answer = gets.chomp.downcase
     break if ['y', 'n'].include?(answer)
@@ -164,24 +156,25 @@ loop do
   display_menu
   print "  SELECT: "
   choice = nil
+
   loop do
     choice = gets.chomp.downcase
     break if ['a', 'b', 'c'].include?(choice)
     prompt("Error! Invalid input.")
     print "  SELECT: "
   end
-  
+
   case choice
   when 'b'
     puts ''
     print ">> Choose the winning number (greater than 21): "
-    special_number = nil
+    special_num = nil
     loop do
-      special_number = gets.chomp
-      break if special_number.to_i.to_s == special_number && special_number.to_i > 21
+      special_num = gets.chomp
+      break if special_num.to_i.to_s == special_num && special_num.to_i > 21
       prompt("Error! Select a number greater than 21.")
     end
-    WINNING_NUMBER = special_number.to_i
+    WINNING_NUMBER = special_num.to_i
   when 'c'
     break
   else
@@ -189,10 +182,10 @@ loop do
   end
 
   loop do
-    scores = {player: 0, dealer: 0}
+    scores = { player: 0, dealer: 0 }
     loop do
       deck = shuffle_deck
-      
+
       p_hand = []
       d_hand = []
       p_status = nil
@@ -205,10 +198,15 @@ loop do
       p_total = cards_total(p_hand)
       d_total = cards_total(d_hand)
 
-      display_screen(p_hand, d_hand, p_status, p_total, d_total, scores)
+      display_scores(scores)
+      display_table(p_hand, d_hand, p_total)
+      round_outcome(p_hand, p_status, p_total, d_total)
 
       loop do
-        display_screen(p_hand, d_hand, p_status, p_total, d_total, scores)
+        display_scores(scores)
+        display_table(p_hand, d_hand, p_total)
+        round_outcome(p_hand, p_status, p_total, d_total)
+
         break if busted?(p_total)
         loop do
           prompt("Choose: (1) HIT or (2) STAY")
@@ -223,7 +221,11 @@ loop do
 
       if busted?(p_total)
         score_keeper(p_total, d_total, scores)
-        round_display_screen(p_hand, d_hand, p_status, p_total, d_total, scores)
+
+        display_scores(scores)
+        reveal_table(p_hand, d_hand, p_total, d_total)
+        round_outcome(p_hand, p_status, p_total, d_total)
+
         break if scores.values.include?(5)
 
         prompt("Press 'Enter' to continue...")
@@ -233,26 +235,25 @@ loop do
         puts "You chose to stay!"
       end
 
-      until d_total >= (WINNING_NUMBER * 0.8).ceil || busted?(d_total)
+      until d_total >= (WINNING_NUMBER * 0.85).floor || busted?(d_total)
         d_hand << deck.pop
         d_total = cards_total(d_hand)
       end
 
       score_keeper(p_total, d_total, scores)
-      round_display_screen(p_hand, d_hand, p_status, p_total, d_total, scores)
+      display_scores(scores)
+      reveal_table(p_hand, d_hand, p_total, d_total)
+      round_outcome(p_hand, p_status, p_total, d_total)
 
       break if scores.values.include?(5)
 
       prompt("Press 'Enter' to continue...")
       continue = gets.chomp
       next if continue
-      
     end
-    game_display_screen(scores)
+    display_gameover(scores)
     play_again? ? next : break
   end
 end
-
+empty_lines
 prompt('Thanks for playing!')
-
-
